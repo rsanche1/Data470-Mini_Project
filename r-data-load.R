d5bf314a1558972b0cab5d351c26893fe0890ac1
Rodrigo <165330453+rsanche1@users.noreply.github.com>
2024-09-22 19:05:46 -0500

    Delete r-data-load.R

--- r-data-load.R
+++
@@ -1,4 +0,0 @@
-con <- DBI::dbConnect(duckdb::duckdb(), dbdir = "my-db.duckdb")
-DBI::dbWriteTable(con, "fm_housing", "FM_Housing_2018_2022_clean.csv", overwrite = TRUE, row.names = FALSE)
-DBI::dbDisconnect(con)
-
